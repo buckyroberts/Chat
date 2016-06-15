a2b1873b34e144067a5a755ae558ad40f01cb99a
Bucky <buckyroberts@gmail.com>
2016-06-15 10:22:19 -0400

    Can load content from external resource

--- src/js/bucky.js
+++ src/js/bucky.js
@@ -4,13 +4,11 @@ function changeContent() {
         "age": 72,
         "colors": ["blue", "yellow"]
     };
-    console.log(sampleJson.name);
     $('.main-area').html('<h1>' + sampleJson.name + '</h1>');
 }
 
 function displayChatWindow() {
     $.get('component/chat-window.html', function (data) {
-        console.log(data);
         $('.main-area').html(data);
         $chatHeight = $(document).height() - $('.top-nav').height() - $('.bottom').height();
         $('.chat-window').css('height', $chatHeight);
